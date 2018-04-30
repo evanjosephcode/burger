581c0aad2f2d87130487ccd366320f62ec857c7f
evanjosephcode <evanseaman89@gmail.com>
2018-04-30 12:05:13 -0500

    added schema and insert queries

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Supreme", true)
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Not so Delicioso", false)
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Burger's Burger", false)
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The People's Berg", false)
+
+
+
+
