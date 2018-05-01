6e78639e02c9b00fd36885400c68d95dae9369af
evanjosephcode <evanseaman89@gmail.com>
2018-05-01 16:46:55 -0500

    added js and css folders in assets

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var port = 3000;
+var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,7 +13,10 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// serve up files in /public folder
+// app.use(express.static(path.join(__dirname, '/public')));
+
+
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
