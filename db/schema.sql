029970d7d8b6d1adcc333d385b7d6d49bd95757a
evanjosephcode <evanseaman89@gmail.com>
2018-05-11 11:32:00 -0500

    added createdAt for schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,9 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-id int NOT NULL AUTO_INCREMENT,
+id INT AUTO_INCREMENT NOT NULL,
 burger_name varchar(255) NOT NULL,
 devoured BOOLEAN NOT NULL DEFAULT FALSE,
+createdAt TIMESTAMP NOT NULL,
 PRIMARY KEY (id)
 );
